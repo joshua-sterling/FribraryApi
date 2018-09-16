f99c2116f49e68f3f23b9e98434a2484de3332f1
jsterling15 <joshua.sterling15@gmail.com>
2018-09-16 15:38:32 -0500

    Add automapper

--- FribraryApi/FribraryApi/Controllers/SongsController.cs
+++ FribraryApi/FribraryApi/Controllers/SongsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using FribraryApi.Models;
 using FribraryApi.Services;
 using Microsoft.AspNetCore.Http;
@@ -15,37 +16,39 @@ namespace FribraryApi.Controllers
     {
 
         private readonly ISongsService _songService;
+        private readonly IMapper _mapper;
 
-        public SongsController(ISongsService songService)
+        public SongsController(ISongsService songService, IMapper mapper)
         {
             _songService = songService;
+            _mapper = mapper;
         }
 
         // GET: api/Songs
         [HttpGet]
         public IActionResult Get()
         {
-            var queryResults = _songService.GetAll();
+            var queryResults = _songService.GetAll().ToList();
 
-            var ResponseModel = new List<SongViewModel>();
+            var ResponseModel = _mapper.Map<List<SongViewModel>>(queryResults);
 
-            if (queryResults.ToList().Count > 0)
-            {
-                foreach (var result in queryResults)
-                {
-                    var newModel = new SongViewModel()
-                    {
-                        Id = result.Id,
-                        Name = result.Name,
-                        Artist = new ArtistViewModel()
-                        {
-                            Id = result.Artist.Id,
-                            Name = result.Artist.Name
-                        }
-                    };
-                    ResponseModel.Add(newModel);
-                }
-            }
+            //if (queryResults.ToList().Count > 0)
+            //{
+            //    foreach (var result in queryResults)
+            //    {
+            //        var newModel = new SongViewModel()
+            //        {
+            //            Id = result.Id,
+            //            Name = result.Name,
+            //            Artist = new ArtistViewModel()
+            //            {
+            //                Id = result.Artist.Id,
+            //                Name = result.Artist.Name
+            //            }
+            //        };
+            //        ResponseModel.Add(newModel);
+            //    }
+            //}
 
             return Ok(ResponseModel);
         }
